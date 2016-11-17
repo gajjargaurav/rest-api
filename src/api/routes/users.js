7c055b63fa7fe75d811b3cedf445cdd3b6a6d629
Gaurav Gajjar <gajjargaurav@gmail.com>
2016-11-17 09:53:01 +0000

    docs(adding comment to highlight how to start addming middlewares):

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const router = express.Router()
 
+// This is the place where middlewares and individual resource middlewares
+// can be piped to achieve the results desiged
+// e.g. .get('/', isLoggedIn, hasAccess, userMiddleware.list)
 router
 .get('/:id', () => {})
 .get('/', (req, res) => res.status(200).json({ status: 'ok', user: true }))
