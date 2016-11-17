c5e4615923628a36ed54eed5cbf7a70323f7a018
Gaurav Gajjar <gajjargaurav@gmail.com>
2016-11-17 15:05:33 +0000

    chore(comments): removeing unwanted comments

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -2,9 +2,6 @@ import express from 'express'
 
 const router = express.Router()
 
-// This is the place where middlewares and individual resource middlewares
-// can be piped to achieve the results desiged
-// e.g. .get('/', isLoggedIn, hasAccess, userMiddleware.list)
 router
 .get('/:id', () => {})
 .get('/', (req, res) => res.status(200).json({ status: 'ok', user: true }))
